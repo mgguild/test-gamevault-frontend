7a004f6b6bc4124519fc46a6631a30190b482659
kojiadrianojr <iisparkplugiiog@gmail.com>
2022-01-18 16:17:39 +0800

    Replace metadata, Remove icon on headers

--- src/views/Farms/Farms.tsx
+++ src/views/Farms/Farms.tsx
@@ -431,7 +431,7 @@ const Farms: React.FC = () => {
            </Flex>
         </InfoBox>
         </Flex>
-          <Flex style={isMobile ? {
+          {/* <Flex style={isMobile ? {
             fontSize: '150px',
             margin: 'auto',
             marginTop: '20px',
@@ -439,7 +439,7 @@ const Farms: React.FC = () => {
           } : { fontSize: '240px', marginRight: '-118px' }}>
             <SvgIcon component={theme.isDark ? FarmsDarkLogo : FarmsLightLogo} viewBox='0  0 384 512'
                      style={isMobile ? { width: '200px' } : { width: '500px' }} fontSize='inherit' />
-          </Flex>
+          </Flex> */}
         </Flex>
       </PageHeader>
       <Page>
