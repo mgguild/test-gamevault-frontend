fc0f58718276a3720f0c9f4d38170ba4a6d8918c
kojiadrianojr <iisparkplugiiog@gmail.com>
2022-02-01 14:37:11 +0800

    feat: Create inactive section file

---
+++ src/views/GuildPad/sections/Inactive/Inactive.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from '@metagg/mgg-uikit'
+
+const Inactive:React.FC = () => {
+    return (
+        <Text>Inactive</Text>
+    )
+}
+
+export default Inactive;
