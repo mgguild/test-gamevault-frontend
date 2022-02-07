0360eea90c6a816e122ed4f359d528b4b9c10402
kojiadrianojr <iisparkplugiiog@gmail.com>
2022-02-08 03:32:40 +0800

    Update mgg guildpad info

--- src/config/constants/Guildpads/MggGuildpad.ts
+++ src/config/constants/Guildpads/MggGuildpad.ts
@@ -1,15 +1,23 @@
-
-import { IGuildpad, STATE } from "../types";
+import { IGuildpad, STATE, TYPE, DISTRIBUTION } from "../types";
 import tokens from '../tokens';
 
+const socials = {
+    website: 'https://www.tankwars.zone/',
+    twitter: 'https://twitter.com/TankWarsZone',
+    telegram: 'https://t.me/TankWarsZone',
+    discord: 'https://discord.com/invite/82jCm7QyRA',
+    medium: 'https://tankwarszone.medium.com/'
+}
 const guildpad: IGuildpad = {
     title: 'MGG',
     description: 'Lorem Ipsum',
-    distribution: 'vesting',
-    status: STATE.upcoming,
+    socials,
+    type: TYPE.INO,
+    distribution: DISTRIBUTION.VESTING,
+    status: STATE.active,
     address: '0x000', // Contract address
     buyingCoin: tokens.bnb,
     sellingCoin: tokens.mgg
 }
 
-export default guildpad
+export default guildpad;
--- src/config/constants/guildpads.ts
+++ src/config/constants/guildpads.ts
@@ -4,7 +4,7 @@ import TankWarsZoneGuildpad from './Guildpads/TankWarsZone';
 import DemoleGuildpad from './Guildpads/Demole';
 
 const Guildpads: IGuildpad[] = [
-    // MggGuildpad, [ disable mgg guildpad ]
+    // MggGuildpad, disable MGG 
     TankWarsZoneGuildpad,
     DemoleGuildpad
 ]
