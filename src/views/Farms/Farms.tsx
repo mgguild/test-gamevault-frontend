d2789a849adcb050a7ed03952591b80fc0b79637
Medard Mandane <medardm13@gmail.com>
2022-02-11 23:16:57 +0800

    Enable APR

--- src/views/Farms/Farms.tsx
+++ src/views/Farms/Farms.tsx
@@ -429,10 +429,11 @@ const Farms: React.FC = () => {
              <Text fontSize='17px' bold color={theme.colors.MGG_accent2}>Total Value Locked</Text>
              <Text fontSize='20px'>- USD</Text>
            </Flex>
-           <Flex flexDirection="column">
-             <Text fontSize='17px' bold color={theme.colors.MGG_accent2}>APR</Text>
-             <Text fontSize='20px'>{apr} % </Text>
-           </Flex> */}
+            */}
+            <Flex flexDirection="column">
+              <Text fontSize='17px' bold color={theme.colors.MGG_accent2}>APR</Text>
+              <Text fontSize='20px'>{apr} % </Text>
+            </Flex>
         </InfoBox>
         </Flex>
           {/* <Flex style={isMobile ? {
