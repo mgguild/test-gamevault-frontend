c1d93b35ea1c8d1c553753c54385cb634feb59a2
kojiadrianojr <iisparkplugiiog@gmail.com>
2022-02-15 13:42:50 +0800

    Update scroll function

--- src/views/GuildPad/Guildpad.tsx
+++ src/views/GuildPad/Guildpad.tsx
@@ -1,31 +1,34 @@
 import React, { useMemo } from 'react'
 import { Text } from '@metagg/mgg-uikit'
-import { useGuildpads } from 'state/hooks';
-import { STATE } from 'config/constants/types';
+import { Element } from 'react-scroll'
+import { useGuildpads } from 'state/hooks'
+import { STATE } from 'config/constants/types'
 import Page from 'components/layout/Page'
-import { AboutSection, ActiveSection, ApplySection, FooterSection, HomeSection, InactiveSection } from './sections';
-
-
-
+import { AboutSection, ActiveSection, ApplySection, FooterSection, HomeSection, InactiveSection } from './sections'
 
 const Guildpad: React.FC = () => {
-    const { data: Guildpads } = useGuildpads()
-    const activeGP = useMemo(() => Guildpads.filter((guildpad) => guildpad.status === STATE.active), [Guildpads])
-    const inactiveGP = useMemo(() => Guildpads.filter((guildpad) => guildpad.status === STATE.upcoming || guildpad.status === STATE.completed), [Guildpads])
-    // const completedGP = useMemo(() => Guildpads.filter((guildpad) => guildpad.status === STATE.completed), [])
+  const { data: Guildpads } = useGuildpads()
+  const activeGP = useMemo(() => Guildpads.filter((guildpad) => guildpad.status === STATE.active), [Guildpads])
+  const inactiveGP = useMemo(
+    () => Guildpads.filter((guildpad) => guildpad.status === STATE.upcoming || guildpad.status === STATE.completed),
+    [Guildpads],
+  )
+  // const completedGP = useMemo(() => Guildpads.filter((guildpad) => guildpad.status === STATE.completed), [])
 
-    return (
-        <>
-            <HomeSection />
-            <Page>
-                <ActiveSection guildpads={activeGP} />
-                <InactiveSection guildpads={inactiveGP} />
-                <AboutSection />
-                <ApplySection />
-            </Page>
-            <FooterSection />
-        </>
-    )
+  return (
+    <>
+      <HomeSection />
+      <Page>
+        <Element name="activeSection">
+          <ActiveSection guildpads={activeGP} />
+        </Element>
+        <InactiveSection guildpads={inactiveGP} />
+        <AboutSection />
+        <ApplySection />
+      </Page>
+      <FooterSection />
+    </>
+  )
 }
 
 export default Guildpad
