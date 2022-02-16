3553003d6ca47a18f39ba7286d835d8a2fd4248e
kojiadrianojr <iisparkplugiiog@gmail.com>
2022-02-16 22:01:31 +0800

    Remove MGG IGO

--- src/config/constants/guildpads.ts
+++ src/config/constants/guildpads.ts
@@ -1,11 +1,9 @@
 import { GuildpadConfig } from "./types";
-import MggGuildpad from "./Guildpads/MggGuildpad";
 import TankWarsZoneGuildpad from './Guildpads/TankWarsZone';
 import DemoleGuildpad from './Guildpads/Demole';
 import AcknoledgerGuildpad from './Guildpads/Acknoledger';
 
 const Guildpads: GuildpadConfig[] = [
-    MggGuildpad,
     TankWarsZoneGuildpad,
     DemoleGuildpad,
     AcknoledgerGuildpad,
