d965bfacd95e3423e0de01a05e5bdb60df6f32ea
Salvador Amba Jr <salvadorjr.amba@gmail.com>
2022-02-18 20:48:26 +0800

    add parnthesis

--- src/views/GuildPad/Pad/BoxCard/BoxCard.tsx
+++ src/views/GuildPad/Pad/BoxCard/BoxCard.tsx
@@ -115,7 +115,7 @@ const ProgressBar: React.FC<{ token: string, guildpad: Guildpad, rarity?: string
         <Text>Price per Box:</Text>
         <JustifyR>
           {/* <BoxImg size="1.8rem" src={`/images/tokens/${token}.svg`} alt="BNB" /> */}
-          <Text>1425 WBOND</Text>{/* Temporary fix */}
+          <Text>(1425 WBOND)</Text>{/* Temporary fix */}
           {/* <Text>{guildpad.boxInfo[rarity].price} BNB</Text> */}
         </JustifyR>
       </ColumnTwo>
