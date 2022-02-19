051a0b688fc2e4baa0e48d37e04dec38e12c923e
Medard Mandane <medardm13@gmail.com>
2022-02-19 19:50:45 +0800

    Adjust configs for round 2
    - prepare Tankwars round 2

--- src/config/constants/Guildpads/TankWarsZone2.ts
+++ src/config/constants/Guildpads/TankWarsZone2.ts
@@ -13,7 +13,7 @@ const guildpad = {
   description: 'Tank wars zone is a metaverse-ready exciting and visually refreshing action game built on blockchain technology. Being the first blockchain game on Fantom & invested by the Fantom Foundation, the game will have very low transactions fees, the lowest time confirmations, and supportive communities for players. With various gameplays, you can play solo, or gather your friends, form a team, and battle with others while being able to earn money just by playing or staking your NFTs.',
   socials,
   type: TYPE.INO,
-  round: '1',
+  round: '2',
   distribution: DISTRIBUTION.AIRDROP,
   status: GUILDPAD_STATUS.ongoing,
   contractAddress: {
