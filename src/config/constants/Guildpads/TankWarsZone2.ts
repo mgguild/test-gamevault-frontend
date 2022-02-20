8679775ad871199895115146dd620f7b391a71a2
Medard Mandane <medardm13@gmail.com>
2022-02-20 23:27:59 +0800

    Mark tankwars round 2 as ended

--- src/config/constants/Guildpads/TankWarsZone2.ts
+++ src/config/constants/Guildpads/TankWarsZone2.ts
@@ -15,7 +15,7 @@ const guildpad = {
   type: TYPE.INO,
   round: '2',
   distribution: DISTRIBUTION.AIRDROP,
-  status: GUILDPAD_STATUS.ongoing,
+  status: GUILDPAD_STATUS.completed,
   contractAddress: {
     97: '0xE673E67bD59A7390725ED98cBead9A5928F6f51E',
     56: '0x00e05c0d19eB1a3B504da862c47282C7Dc046934',
