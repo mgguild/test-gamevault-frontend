4b6c56d4e1203ab2d22114db242a558b0c61c5f8
Salvador Amba Jr <salvadorjr.amba@gmail.com>
2022-02-20 21:53:08 +0800

    TWZ:R2 change to completed, added prop type isSoldOut

--- src/config/constants/types.ts
+++ src/config/constants/types.ts
@@ -225,6 +225,7 @@ interface GuildPadInformation {
   type?: Type;
   date?: {start: string, end: string};
   status: Status;
+  isSoldOut?: boolean;
   hasStarted?: boolean;
   hasEnded?: boolean;
   totalSupply?: string;
