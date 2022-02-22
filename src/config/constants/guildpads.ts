0f97eb4b1329984a63775ba648d0090ebe96304b
Salvador Amba Jr <salvadorjr.amba@gmail.com>
2022-02-23 02:05:45 +0800

    Guildpads config changes

--- src/config/constants/guildpads.ts
+++ src/config/constants/guildpads.ts
@@ -3,11 +3,18 @@ import TankWarsZoneGuildpad from './Guildpads/TankWarsZone'
 import TankWarsZoneGuildpad2 from './Guildpads/TankWarsZone2'
 import DemoleGuildpad from './Guildpads/Demole'
 
+// Made ids equal to it's index for mergingGuildpads else it wont work
 const Guildpads: GuildpadConfig[] = [
+  {
+    id: 0,
+    title: 'TankWars Zone',
+    nextRoundID: 1,
+    ... TankWarsZoneGuildpad
+  },
   {
     id: 1,
     title: 'TankWars Zone',
-    ... TankWarsZoneGuildpad
+    ... TankWarsZoneGuildpad2
   },
   {
     id: 2,
