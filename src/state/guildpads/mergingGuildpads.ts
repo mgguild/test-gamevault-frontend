4a57b5180021904a93a5b9420259e5297d50a486
Salvador Amba Jr <salvadorjr.amba@gmail.com>
2022-02-24 20:59:34 +0800

    Additional condition rule and property merge

--- src/state/guildpads/mergingGuildpads.ts
+++ src/state/guildpads/mergingGuildpads.ts
@@ -1,23 +1,34 @@
 /* eslint-disable no-param-reassign */
 
 import BigNumber from 'bignumber.js'
-import { Guildpad, GuildpadState } from '../types'
+import { GUILDPAD_STATUS } from 'config/constants/types'
+import { Guildpad } from '../types'
 
 
 const mergingGuildpads = async (guildpads: Guildpad[]): Promise<Guildpad[]> => {
 
   guildpads.forEach((guildpad, index) => {
-    // Find toMergeID property exists
-    if(guildpad.toMergeID){
-      guildpads[index].round = 'MERGED'
+    // Find toMergeID property exists and check if ended
+    if(guildpad.toMergeID && guildpad.hasEnded && guildpad.status === GUILDPAD_STATUS.completed){
+
       const toMergeGP = guildpads.filter((gp) => gp.id === guildpad.toMergeID)[0]
 
-      Object.keys(guildpad).forEach((key) => {
-        if(key === 'totalSold' || key === 'totalRaise'){
-          // Sum of totalSold and totalsRaise of from rounds
-          guildpads[index][key] = new BigNumber(guildpad[key]).plus(new BigNumber(toMergeGP[key])).toString()
-        }
-      })
+      if(toMergeGP.hasEnded && toMergeGP.status === GUILDPAD_STATUS.completed){
+        Object.keys(guildpad).forEach((key) => {
+          if(key === 'totalSold' || key === 'totalRaise'){
+            // Sum of totalSold and totalsRaise of from rounds
+            guildpads[index][key] = new BigNumber(guildpad[key]).plus(new BigNumber(toMergeGP[key])).toString()
+          }
+          if(key === 'epochEndDate'){
+            // get latest round epochEndDate
+            guildpads[index][key] = toMergeGP[key]
+          }
+        })
+        guildpads[index].round = 'MERGED'
+        guildpads[index].display = true // auto apply if merge successful
+      }else{
+        guildpads[index].display = false
+      }
     }
   })
 
