8cfc1afefc0302a2eb3ad54f116cf31656af6c13
Medard Mandane <medardm13@gmail.com>
2022-02-28 14:31:30 +0800

    Add round 2 indicator in launchpad config

--- src/config/constants/guildpads.ts
+++ src/config/constants/guildpads.ts
@@ -13,7 +13,7 @@ const Guildpads: GuildpadConfig[] = [
   },
   {
     id: 2,
-    title: 'TankWars Zone',
+    title: 'TankWars Zone (Round 2)',
     ... TankWarsZoneGuildpad2
   },
   { // MERGE HOLDER
