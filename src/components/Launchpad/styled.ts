e6d8725ca2434770aa2d39e24878e228fc1178f5
Salvador Amba Jr <salvadorjr.amba@gmail.com>
2022-03-02 19:06:31 +0800

    darken bg guildpad post header on light mode

--- src/components/Launchpad/styled.ts
+++ src/components/Launchpad/styled.ts
@@ -70,7 +70,8 @@ export const PostHeader = styled(Header)<{background?:string}>`
   // background-repeat: no-repeat;
   border-top: 3px solid ${(({theme}) => theme.colors.MGG_active)};
   border-bottom: 3px solid ${(({theme}) => theme.colors.MGG_active)};
-  ${({ background }) =>
+  background: #101010;
+  ${({ background, theme }) =>
   background &&
   `
       &:before {
@@ -79,7 +80,7 @@ export const PostHeader = styled(Header)<{background?:string}>`
           position: absolute;
           left: 0;
           top: 0;
-          opacity: 0.2;
+          opacity: ${theme.isDark ? 0.2 : 0.5};
           width: 100%;
           height: 100%;
           z-index: 1;
