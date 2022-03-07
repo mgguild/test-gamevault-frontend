fd895e0d9ee5e22f5fb086921193733b93fa6bd2
Salvador Amba Jr <salvadorjr.amba@gmail.com>
2022-03-07 16:11:43 +0800

    UI tests

--- src/views/Farms/NewUI.tsx
+++ src/views/Farms/NewUI.tsx
@@ -11,44 +11,69 @@ const getImageUrlFromToken = (token: Token) => {
   return `/images/tokens/${address}.${token.iconExtension?? 'svg'}`
 }
 
+const StakeCards = styled.div<{src? : string, bgColor? : string}>`
+  position: relative;
+  min-width: 35rem;
+  min-height: 15rem;
+  background-color: ${({bgColor}) => bgColor ?? '#a30404b8'};
+  ${({src, bgColor}) => src &&
+    `&:before {
+      content: '';
+      position: absolute;
+      left: 0;
+      top: 0;
+      opacity: 0.2;
+      width: 100%;
+      height: 100%;
+      z-index: 0;
+      background-image: url(${src});
+      background-repeat: no-repeat;
+      // background-attachment: fixed;
+      background-position: center;
+      background-size: cover;
+    }`
+  }
+`
+
+const StakeCardContainer = styled.div`
+  position: relative;
+  padding: 0.3rem 1rem;
+  background: linear-gradient(0deg,rgb(0 0 0) 0%,rgb(0 0 0 / 50%) 25%,rgba(36,121,9,0) 75%);
+  width: 100%;
+  height: 100%;
+  z-index: 2;
+`
+
 const NewFarms: React.FC = () => {
   return (
     <>
       <div style={{
-        padding: '5rem'
+        padding: '5rem',
       }}>
         <Text>
           NEW FARMS
         </Text>
         <div style={{
           display: 'flex',
-          flexDirection: 'row',
-          columnGap: '2rem'
+          flexFlow: 'row wrap',
+          justifyContent: 'space-evenly',
+          columnGap: '2rem',
+          rowGap: '2rem'
         }}>
-          <div style={{
-            padding: '0.3rem 1rem',
-            borderStyle: 'solid',
-            borderColor: 'red',
-            borderWidth: 'thin',
-            minWidth: '5rem',
-            minHeight: '3rem'
-          }}>
-            <Text>
-              test
-            </Text>
-          </div>
-          <div style={{
-            padding: '0.3rem 1rem',
-            borderStyle: 'solid',
-            borderColor: 'red',
-            borderWidth: 'thin',
-            minWidth: '5rem',
-            minHeight: '3rem'
-          }}>
-            <Text>
-              test
-            </Text>
-          </div>
+          <StakeCards src='./MGG.png' bgColor='#030f62'>
+            <StakeCardContainer>
+              <Text>
+                test
+              </Text>
+            </StakeCardContainer>
+          </StakeCards>
+          <StakeCards src='./MGG.png'>
+            <StakeCardContainer>
+              <Text>
+                test
+              </Text>
+            </StakeCardContainer>
+          </StakeCards>
         </div>
       </div>
     </>
