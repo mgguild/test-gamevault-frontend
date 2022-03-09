e2c40224fa7ef8b2fad7f2190b14c6801cc095dd
Salvador Amba Jr <salvadorjr.amba@gmail.com>
2022-03-09 15:24:09 +0800

    add FX styles farms setup

--- src/views/Farms/NewUI.tsx
+++ src/views/Farms/NewUI.tsx
@@ -1,22 +1,60 @@
-import React from 'react'
-import { Text, Flex, Image, RowType, Toggle } from '@metagg/mgg-uikit'
-import PageHeader from 'components/PageHeader'
-import styled from 'styled-components'
+import React, { useCallback, useContext, useEffect, useMemo, useRef, useState } from 'react'
+import { Route, useLocation, useRouteMatch } from 'react-router-dom'
+import BigNumber from 'bignumber.js'
+import { useWeb3React } from '@web3-react/core'
+import { Flex, Image, RowType, Toggle } from '@pancakeswap/uikit'
+import { Oval } from 'react-loading-icons'
+import { Text, Heading } from '@metagg/mgg-uikit'
+import styled, { ThemeContext } from 'styled-components'
+import FlexLayout from 'components/layout/Flex'
+import Page from 'components/layout/Page'
+import useMedia from 'use-media'
+import { SvgIcon } from '@material-ui/core'
+import { useFarms, usePollFarmsData, usePriceCakeBusd } from 'state/hooks'
+import usePersistState from 'hooks/usePersistState'
+import { useFarmPrice } from 'hooks/price'
+import usePrevious from 'utils/refHelpers'
+import { Farm } from 'state/types'
+import { useTranslation } from 'contexts/Localization'
+import { getBalanceNumber, getBalanceAmount } from 'utils/formatBalance'
+import { getFarmApr, getFarmV2Apr } from 'utils/apr'
+import useTokenBalance from 'hooks/useTokenBalance'
+import { orderBy } from 'lodash'
 import tokens from 'config/constants/tokens'
 import { Token } from 'config/constants/types'
 import { getAddress } from 'utils/addressHelpers'
+import isArchivedPid from 'utils/farmHelpers'
+import { latinise } from 'utils/latinise'
+import PageHeader from 'components/PageHeader'
+import SearchInput from 'components/SearchInput'
+import Select, { OptionProps } from 'components/Select/Select'
+import FarmCard, { FarmWithStakedValue } from './components/FarmCard/FarmCard'
+import Table from './components/FarmTable/FarmTable'
+import FarmTabButtons from './components/FarmTabButtons'
+import { RowProps } from './components/FarmTable/Row'
+import { DesktopColumnSchema, ViewMode } from './components/types'
+import { ReactComponent as FarmsDarkLogo } from './components/assets/farm-dark.svg'
+import { ReactComponent as FarmsLightLogo } from './components/assets/farm-light.svg'
+
+import '../../css/styleFX.css'
 
 const getImageUrlFromToken = (token: Token) => {
   const address = getAddress(token.symbol === 'BNB' ? tokens.wbnb.address : token.address)
   return `/images/tokens/${address}.${token.iconExtension?? 'svg'}`
 }
 
-const StakeCards = styled.div<{src? : string, bgColor? : string}>`
+const StakeCards = styled.button<{src? : string, bgColor? : string}>`
   position: relative;
-  min-width: 35rem;
-  min-height: 15rem;
+  min-width: 20rem;
+  max-width: 40rem;
+  min-height: 12rem;
+  cursor: pointer;
+  border: none;
+  padding: 0;
+  text-align: left;
+  flex: 1;
   background-color: ${({bgColor}) => bgColor ?? '#a30404b8'};
-  ${({src, bgColor}) => src &&
+  ${({src}) => src &&
     `&:before {
       content: '';
       position: absolute;
@@ -36,23 +74,54 @@ const StakeCards = styled.div<{src? : string, bgColor? : string}>`
 `
 
 const StakeCardContainer = styled.div`
+  display: grid;
   position: relative;
-  padding: 0.3rem 1rem;
+  padding: 1rem 1.5rem;
   background: linear-gradient(0deg,rgb(0 0 0) 0%,rgb(0 0 0 / 50%) 25%,rgba(36,121,9,0) 75%);
   width: 100%;
   height: 100%;
   z-index: 2;
+  grid-template-columns: 1fr 1fr;
+`
+const TokenLogo = styled.img<{ size?: string }>`
+  border-radius: 50%;
+  height: ${({ size }) => (!size ? '80px' : size)};
+  width: ${({ size }) => (!size ? '80px' : size)};
+  z-index: 2;
+  @media (max-width: 500px) {
+    height: ${({ size }) => (!size ? '50px' : size)};
+    width: ${({ size }) => (!size ? '50px' : size)};
+  }
 `
 
+const Badge = styled.div<{ type?: number}>`
+  ${({type}) => type ?
+    `
+      padding: 0.2rem 0.5rem;
+      border: thin solid white;
+      border-radius: 0.3rem;
+    `
+    :
+    `
+      padding: 0.2rem 0.5rem;
+      background-color: rgb(0,196,204);
+      border-radius: 0.3rem;
+    `
+  }
+`
+
+const NUMBER_OF_FARMS_VISIBLE = 12
+
 const NewFarms: React.FC = () => {
+
   return (
     <>
       <div style={{
         padding: '5rem',
       }}>
-        <Text>
-          NEW FARMS
-        </Text>
+        <Heading size='xl'>
+          Live Farms
+        </Heading>
         <div style={{
           display: 'flex',
           flexFlow: 'row wrap',
@@ -60,14 +129,33 @@ const NewFarms: React.FC = () => {
           columnGap: '2rem',
           rowGap: '2rem'
         }}>
-          <StakeCards src='./MGG.png' bgColor='#030f62'>
+          <StakeCards src='./MGG.png' bgColor='#030f62' className='shodow-pop'>
             <StakeCardContainer>
-              <Text>
-                test
-              </Text>
+              <TokenLogo size='3.5rem' src={getImageUrlFromToken(tokens.mgg)} />
+              <Flex style={{
+                flexFlow: 'row wrap',
+                columnGap: '0.5rem',
+                justifyContent: 'end',
+              }}>
+                <div><Badge><Text color='white'>New</Text></Badge></div>
+                <TokenLogo size='2rem' src={getImageUrlFromToken(tokens.wbnb)} />
+                <div><Badge type={1}><Text color='white'>LP FARM</Text></Badge></div>
+              </Flex>
+              <Flex style={{alignItems: 'end'}}>
+                <div>
+                  <Text color='white'>MGG Token</Text>
+                  <Heading color='white'>Metagaimng Guild</Heading>
+                </div>
+              </Flex>
+              <Flex style={{justifyContent: 'end', alignItems: 'end'}}>
+                <div style={{textAlign: 'end'}}>
+                  <Text color='white'>APY 150%</Text>
+                  <Heading color='white'>100 Days Left</Heading>
+                </div>
+              </Flex>
             </StakeCardContainer>
           </StakeCards>
-          <StakeCards src='./MGG.png'>
+          <StakeCards src='./MGG.png' className='shodow-pop'>
             <StakeCardContainer>
               <Text>
                 test
