978701c97c4906253661337f6354772a39df3318
Medard Mandane <medardm13@gmail.com>
2022-03-10 21:14:24 +0800

    Made round dynamic in IGO page

--- src/views/GuildPad/Pad/Cards/Ido.tsx
+++ src/views/GuildPad/Pad/Cards/Ido.tsx
@@ -80,7 +80,7 @@ const IdoCard: React.FC<{ guildpad: Guildpad; userDataLoaded: boolean }> = ({ gu
     <ContainerBoxCard>
       <Grid container spacing={2}>
         <Grid item xs={4} md={6}>
-          <CountDown status={guildpad.status} round='1' start end={guildpad.epochEndDate} />
+          <CountDown status={guildpad.status} round={guildpad.round} start end={guildpad.epochEndDate} />
           <Text color='rgba(216, 209, 232, 1)' fontSize='17px' padding='10px 0px 0px 0px'
                 margin='10px 0px 0px 0px'>{details}</Text>
         </Grid>
