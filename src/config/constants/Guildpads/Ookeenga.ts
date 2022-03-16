15febbf164bac06cc5aaa13f312b3a726d4014d7
Salvador Amba Jr <salvadorjr.amba@gmail.com>
2022-03-16 18:03:40 +0800

    OKG description fix

--- src/config/constants/Guildpads/Ookeenga.ts
+++ src/config/constants/Guildpads/Ookeenga.ts
@@ -8,7 +8,7 @@ const socials = {
   youtube: 'https://www.youtube.com/channel/UCqQpViSkkAq_aFP3hj3GL8w'
 }
 const guildpad = {
-  description: 'Ookeenga (OKG) is 3D NFT gaming project which combines blockchain technology with breath-taking graphics, appealing world-building, and addictive gameplay to create a uniquely immersive play-to-earn experience developed by CROS Gamestudio and published by SPORES Network. <br/><br/> OKG is set in a world where insects have evolved and built a massive civilization in an ancient forest called Glaik (The Sacred Forest), which divided into 2 factions: the Akhah (Pureblood) and the Ahika (Mixedblood).',
+  description: 'Ookeenga (OKG) is 3D NFT gaming project which combines blockchain technology with breath-taking graphics, appealing world-building, and addictive gameplay to create a uniquely immersive play-to-earn experience developed by CROS Gamestudio and published by SPORES Network. OKG is set in a world where insects have evolved and built a massive civilization in an ancient forest called Glaik (The Sacred Forest), which divided into 2 factions: the Akhah (Pureblood) and the Ahika (Mixedblood).',
   socials,
   type: TYPE.IDO,
   round: '1',
