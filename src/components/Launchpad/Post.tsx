213b374035e1f97311f01a287205de13a0431515
kojiadrianojr <iisparkplugiiog@gmail.com>
2022-03-28 19:15:57 +0800

    Run prettier format

--- src/components/Launchpad/Post.tsx
+++ src/components/Launchpad/Post.tsx
@@ -59,7 +59,8 @@ const Post: React.FC<{ guildpad?: Guildpad }> = ({ guildpad }) => {
     new BigNumber(guildpad.totalSupply).minus(new BigNumber(guildpad.totalSold)),
   ).isGreaterThan(0)
   const hasToClaimNow = toBigNumber(guildpad.userData.vesting.availableToClaim).gt(0)
-  const USER_CLAIMABLE: 'USER_CLAIMABLE' | 'NOT_USER_CLAIMABLE' = guildpad.userData.vesting.hasClaimable || hasToClaimNow ? 'USER_CLAIMABLE' : 'NOT_USER_CLAIMABLE'
+  const USER_CLAIMABLE: 'USER_CLAIMABLE' | 'NOT_USER_CLAIMABLE' =
+    guildpad.userData.vesting.hasClaimable || hasToClaimNow ? 'USER_CLAIMABLE' : 'NOT_USER_CLAIMABLE'
   return (
     <PostContainer style={{ position: 'relative', overflow: 'hidden' }}>
       <PostHeader background={srcs}>
