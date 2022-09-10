8f218c92609f7181bd5f2db32d25cde02d962476
Koji Adriano Jr <43143132+kojiadrianojr@users.noreply.github.com>
2022-09-10 13:43:29 +0800

    Remove 'token' on title

--- src/views/Gamefi/NewUI/sections/PoolSection.tsx
+++ src/views/Gamefi/NewUI/sections/PoolSection.tsx
@@ -229,7 +229,7 @@ const RenderPool: React.FC<{ farmID: string; tblColumns: any }> = ({ farmID, tbl
               <Flex style={{ alignItems: 'center', justifyContent: 'center' }}>
                 <TokenLogo size="3rem" src={getImageUrlFromToken(currentPool.stakingToken)} />
                 <Heading color="white" style={{ fontSize: '1.5rem', padding: '0 1rem' }}>
-                  {currentPool.name} Token
+                  {currentPool.name}
                 </Heading>
               </Flex>
               <Text color="white">Stake your {currentPool.stakingToken.symbol} tokens for great benefits</Text>
