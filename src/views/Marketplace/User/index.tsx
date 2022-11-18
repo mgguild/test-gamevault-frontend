0bd9e335df9e4023ccdc42f7583e521d4a0db9ab
kojiadrianojr <iisparkplugiiog@gmail.com>
2022-11-18 16:26:48 +0800

    Add background to user profile boilerplate

--- src/views/Marketplace/User/index.tsx
+++ src/views/Marketplace/User/index.tsx
@@ -1,11 +1,17 @@
+import useTheme from 'hooks/useTheme'
 import React from 'react'
-
+import { BgPage, Section } from '../styled'
 
 const UserProfile: React.FC = () => {
+  const { theme } = useTheme()
   return (
-    <div>
+    <Section  padding="0" bgColor={theme.isDark ? '#140937' : theme.colors.MGG_container}>
+      <BgPage>
+        <div style={{ margin: '10rem 0', position: 'relative', zIndex: 2, minHeight: '50vh' }}>
       User Profile
-    </div>
+      </div>
+      </BgPage>
+    </Section>
   )
 }
 
