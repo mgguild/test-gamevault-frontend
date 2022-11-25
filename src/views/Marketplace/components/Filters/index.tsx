033f07cf0ffb815839f54d2c706393d4826ed17c
kojiadrianojr <iisparkplugiiog@gmail.com>
2022-11-25 17:45:56 +0800

    Adjust filter controllers

--- src/views/Marketplace/components/Filters/index.tsx
+++ src/views/Marketplace/components/Filters/index.tsx
@@ -1,4 +1,5 @@
 import { Button, Flex, IconButton, Text } from '@metagg/mgg-uikit'
+import { Grid } from '@mui/material'
 import React, { useState } from 'react'
 import { useMedia } from 'use-media'
 import useTheme from 'hooks/useTheme'
@@ -15,11 +16,11 @@ import SelectComponent from '../Select'
 const Filters = ({ children }: { children?: React.ReactNode }) => {
   const { theme } = useTheme()
   const [boxValue, setBoxValue] = useState({})
-  const [rangeValue, setRangeValue] = useState({min: 0, max: 100, value: 0});
-  const mobileS = useMedia({maxWidth: 375});
-  const [ searchFilter, setSearchFilter ] = useState<string>('')
+  const [rangeValue, setRangeValue] = useState({ min: 0, max: 100, value: 0 })
+  const mobileS = useMedia({ maxWidth: 375 })
+  const [searchFilter, setSearchFilter] = useState<string>('')
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchFilter(e.target.value);
+    setSearchFilter(e.target.value)
   }
   const handleClear = () => {
     setBoxValue({})
@@ -29,8 +30,8 @@ const Filters = ({ children }: { children?: React.ReactNode }) => {
   }
 
   const handleRangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setRangeValue({...rangeValue, [e.target.name]: e.target.value})
-  } 
+    setRangeValue({ ...rangeValue, [e.target.name]: e.target.value })
+  }
 
   return (
     <Container>
@@ -58,7 +59,7 @@ const Filters = ({ children }: { children?: React.ReactNode }) => {
               </Text>
             </AccordionSummary>
             <AccordionDetails>
-              <Checkbox items={['On Sale', 'Verified']} handleCheck={handleCheck} boxValue={boxValue}/>
+              <Checkbox items={['On Sale', 'Verified']} handleCheck={handleCheck} boxValue={boxValue} />
             </AccordionDetails>
           </Accordion>
           <Accordion
@@ -69,7 +70,7 @@ const Filters = ({ children }: { children?: React.ReactNode }) => {
               boxShadow: 'none',
             }}
           >
-            <AccordionSummary expandIcon={<ChevronDown  size="1.75em" color={theme.colors.MGG_accent2} />}>
+            <AccordionSummary expandIcon={<ChevronDown size="1.75em" color={theme.colors.MGG_accent2} />}>
               <Text fontSize="1.5em" bold>
                 PRICE
               </Text>
@@ -77,13 +78,29 @@ const Filters = ({ children }: { children?: React.ReactNode }) => {
             <AccordionDetails>
               <Flex justifyContent="space-between">
                 <div style={{ width: '45%' }}>
-                  <Input handleChange={handleRangeInput} name="min" defaultValue={rangeValue.min} min={rangeValue.min} />
+                  <Input
+                    handleChange={handleRangeInput}
+                    name="min"
+                    defaultValue={rangeValue.min}
+                    min={rangeValue.min}
+                  />
                 </div>
                 <div style={{ width: '45%' }}>
-                  <Input handleChange={handleRangeInput} name="max" defaultValue={rangeValue.max} max={rangeValue.max} />
+                  <Input
+                    handleChange={handleRangeInput}
+                    name="max"
+                    defaultValue={rangeValue.max}
+                    max={rangeValue.max}
+                  />
                 </div>
               </Flex>
-              <Input name="value" handleChange={handleRangeInput} inputType="range" min={rangeValue.min} max={rangeValue.max} />
+              <Input
+                name="value"
+                handleChange={handleRangeInput}
+                inputType="range"
+                min={rangeValue.min}
+                max={rangeValue.max}
+              />
             </AccordionDetails>
           </Accordion>
           <Accordion
@@ -94,14 +111,14 @@ const Filters = ({ children }: { children?: React.ReactNode }) => {
               boxShadow: 'none',
             }}
           >
-            <AccordionSummary expandIcon={<ChevronDown  size="1.75em" color={theme.colors.MGG_accent2} />}>
+            <AccordionSummary expandIcon={<ChevronDown size="1.75em" color={theme.colors.MGG_accent2} />}>
               <Text fontSize="1.5em" bold>
                 SEARCH
               </Text>
             </AccordionSummary>
             <AccordionDetails>
-              <div style={{marginBottom: '1rem'}}>
-              <SearchField handleChange={handleSearch} />
+              <div style={{ marginBottom: '1rem' }}>
+                <SearchField handleChange={handleSearch} />
               </div>
             </AccordionDetails>
           </Accordion>
@@ -112,27 +129,43 @@ const Filters = ({ children }: { children?: React.ReactNode }) => {
               boxShadow: 'none',
             }}
           >
-            <AccordionSummary expandIcon={<ChevronDown  size="1.75em" color={theme.colors.MGG_accent2} />}>
+            <AccordionSummary expandIcon={<ChevronDown size="1.75em" color={theme.colors.MGG_accent2} />}>
               <Text fontSize="1.5em" bold>
                 BLOCKCHAINS
               </Text>
             </AccordionSummary>
             <AccordionDetails>
-              <Checkbox items={['Ethereum', 'BNB', 'Polygon', 'Avalanche', 'Moonriver', 'Moonbeam']} handleCheck={handleCheck} boxValue={boxValue}/>
+              <Checkbox
+                items={['Ethereum', 'BNB', 'Polygon', 'Avalanche', 'Moonriver', 'Moonbeam']}
+                handleCheck={handleCheck}
+                boxValue={boxValue}
+              />
             </AccordionDetails>
           </Accordion>
         </FilterCard>
       </FilterContainer>
       <Main>
-        <Flex flexDirection='column' flexWrap='wrap'>
-        <FilterMenu justify='flex-end'>
-          {
-            !mobileS && ( <Text fontSize='1.75em' bold> Sort by </Text> )
-          }
-          <SelectComponent />
-          <IconButton style={{borderRadius: '50px'}}><RotateCw color='black'/></IconButton>
-        </FilterMenu>
-        {children}
+        <Flex flexDirection="column" flexWrap="wrap">
+          <Grid container marginBottom='2rem'>
+            <Grid container item spacing={2} justifyContent='flex-end'>
+              <Grid item alignItems="center">
+                {!mobileS && (
+                  <Text fontSize="1.75em" bold>
+                    Sort by
+                  </Text>
+                )}
+              </Grid>
+              <Grid item>
+                <SelectComponent />
+              </Grid>
+              <Grid item>
+                <IconButton style={{ borderRadius: '50px' }}>
+                  <RotateCw color="black" />
+                </IconButton>
+              </Grid>
+            </Grid>
+          </Grid>
+          {children}
         </Flex>
       </Main>
     </Container>
