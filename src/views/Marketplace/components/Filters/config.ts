ca9f1b7d96b51c129fe8900a894ca37db25036e9
kojiadrianojr <iisparkplugiiog@gmail.com>
2022-12-02 13:55:09 +0800

    Fix checkbox details on filters

--- src/views/Marketplace/components/Filters/config.ts
+++ src/views/Marketplace/components/Filters/config.ts
@@ -1,35 +1,42 @@
-// Temporary data config 
+// Temporary data config
 
 export const Status1 = [
   {
-    name: 'On Sale'
+    name: 'On Sale',
   },
   {
-    name: 'Verified'
-  }
+    name: 'Verified',
+  },
 ]
 
 export const Status2 = [
   {
     name: 'LoremIpsum1',
-    isVerified: true
+    isVerified: true,
   },
   {
     name: 'LoremIpsum2',
-    isVerified: true
+    isVerified: true,
   },
   {
     name: 'LoremIpsum3',
-    isVerified: true
+    isVerified: true,
   },
   {
     name: 'LoremIpsum4',
-    isVerified: true
+    isVerified: true,
   },
   {
     name: 'LoremIpsum5',
-    isVerified: true
+    isVerified: true,
   },
 ]
 
-export const BlockChain = [{name: 'Ethereum'},{name: 'BNB'}, {name: 'Polygon'}, {name: 'Avalanche'}, {name: 'Moonriver'}, {name: 'Moonbeam'}]
+export const BlockChain = [
+  { name: 'Ethereum' },
+  { name: 'BNB' },
+  { name: 'Polygon' },
+  { name: 'Avalanche' },
+  { name: 'Moonriver' },
+  { name: 'Moonbeam' },
+]
