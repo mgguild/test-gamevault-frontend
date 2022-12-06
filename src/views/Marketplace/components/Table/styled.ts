e4ec1651efe1578029b41f2284f3770fab7d6637
kojiadrianojr <iisparkplugiiog@gmail.com>
2022-12-06 14:05:54 +0800

    Fix table columns width size

--- src/views/Marketplace/components/Table/styled.ts
+++ src/views/Marketplace/components/Table/styled.ts
@@ -20,18 +20,19 @@ export const StyledTableBody = styled.tbody`
   color: ${({ theme }) => theme.colors.text};
 `
 export const StyledTr = styled.tr`
- 
   font-size: 10px;
   ${({ theme }) => theme.mediaQueries.md} {
     font-size: 17px;
   }
 `
 export const StyledTd = styled.td`
+  width: 1%;
   padding: 1.5rem 0;
   font-size: 1em;
   color: ${({theme}) => theme.colors.text};
 `
 export const StyledTh = styled.th`
+width: 1%;
 padding: 1rem 0;
-  font-size: 1.5em;
+font-size: 1.5em;
 `
